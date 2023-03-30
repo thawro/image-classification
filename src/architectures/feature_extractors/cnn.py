7e0e534e95ae61d282a47c23b14a5bdf1024337e
shate <tomashawro.kontakt@gmail.com>
2023-03-30 15:56:15 +0200

    Added docstrings for mlp and deep cnn feature extractors

--- src/architectures/feature_extractors/cnn.py
+++ src/architectures/feature_extractors/cnn.py
@@ -1,9 +1,12 @@
 import torch
 from torch import nn
 from .base import FeatureExtractor
+from typing import Literal
 
 
 class CNNBlock(nn.Module):
+    """Single CNN block constructed of combination of Conv2d, Activation, Pooling, Batch Normalization and Dropout."""
+
     def __init__(
         self,
         in_channels: int,
@@ -12,19 +15,36 @@ class CNNBlock(nn.Module):
         stride: int = 1,
         padding: int | str = 0,
         pool_kernel_size: int = 1,
+        pool_type: Literal["Max", "Avg"] = "Max",
         use_batch_norm: bool = True,
         dropout: float = 0,
-    ) -> None:
+        activation: str = "ReLU",
+    ):
+        """
+        Args:
+            in_channels (int): Number of Conv2d input channels.
+            out_channels (int): Number of Conv2d out channels.
+            kernel_size (int): Conv2d kernel equal to `(kernel_size, kernel_size)`.
+            stride (int, optional): Conv2d stride equal to `(stride, stride)`.
+                Defaults to 1.
+            padding (int | str, optional): Conv2d padding equal to `(padding, padding)`.
+                Defaults to 1.. Defaults to 0.
+            pool_kernel_size (int, optional): Pooling kernel equal to `(pool_kernel_size, pool_kernel_size)`.
+                 Defaults to 1.
+            pool_type (Literal["Max", "Avg"], optional): Pooling type. Defaults to "Max".
+            use_batch_norm (bool, optional): Whether to use Batch Normalization (BN) after activation. Defaults to True.
+            dropout (float, optional): Dropout probability (used after BN). Defaults to 0.
+            activation (str, optional): Type of activation function used before BN. Defaults to 0.
+        """
         super().__init__()
         self.use_pool = pool_kernel_size > 1
         self.use_batch_norm = use_batch_norm
         self.use_dropout = dropout > 0
 
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        self.relu = nn.ReLU()
-
+        self.activation = getattr(nn, activation)()
         if self.use_pool:
-            self.max_pool = nn.MaxPool2d(pool_kernel_size)
+            self.pool = getattr(nn, f"{pool_type}Pool2d")(pool_kernel_size)
 
         if self.use_batch_norm:
             self.batch_norm = nn.BatchNorm2d(out_channels)
@@ -34,9 +54,9 @@ class CNNBlock(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.conv(x)
-        out = self.relu(out)
+        out = self.activation(out)
         if self.use_pool:
-            out = self.max_pool(out)
+            out = self.pool(out)
         if self.use_batch_norm:
             out = self.batch_norm(out)
         if self.use_dropout:
@@ -45,6 +65,8 @@ class CNNBlock(nn.Module):
 
 
 class DeepCNN(FeatureExtractor):
+    """Deep Convolutional Neural Network (CNN) constructed of many CNN blocks and ended with Global Average Pooling."""
+
     name: str = "DeepCNN"
 
     def __init__(
@@ -53,15 +75,37 @@ class DeepCNN(FeatureExtractor):
         out_channels: list[int],
         kernels: list[int],
         pool_kernels: list[int],
+        pool_type: Literal["Max", "Avg"] = "Max",
+        use_batch_norm: bool = True,
+        dropout: float = 0,
+        activation: str = "ReLU",
     ):
+        """
+        Args:
+            in_channels (int): Number of image channels.
+            out_channels (list[int]): Number of channels used in CNN blocks.
+            kernels (list[int]): Kernels of Conv2d in CNN blocks.
+            pool_kernels (list[int]): Kernels of Pooling in CNN blocks.
+            pool_type (Literal["Max", "Avg"], optional): Pooling type in CNN blocks. Defaults to "Max".
+            use_batch_norm (bool, optional): Whether to use BN in CNN blocks. Defaults to True.
+            dropout (float, optional): Dropout probability used in CNN blocks. Defaults to 0.
+            activation (str, optional): Type of activation function used in CNN blocks. Defaults to 0.
+        """
         super().__init__()
         self.out_channels = out_channels
+        fixed_params = dict(
+            pool_type=pool_type,
+            use_batch_norm=use_batch_norm,
+            dropout=dropout,
+            activation=activation,
+        )
         layers = [
             CNNBlock(
                 in_channels if i == 0 else out_channels[i - 1],
                 out_channels[i],
                 kernels[i],
                 pool_kernel_size=pool_kernels[i],
+                **fixed_params,
             )
             for i in range(len(out_channels))
         ] + [nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten()]
--- src/architectures/feature_extractors/mlp.py
+++ src/architectures/feature_extractors/mlp.py
@@ -4,13 +4,30 @@ from .base import FeatureExtractor
 
 
 class FeedForwardBlock(nn.Module):
-    def __init__(self, in_dim: int, out_dim: int, use_batch_norm: bool = True, dropout: float = 0):
+    """Single FeedForward block constructed of combination of Linear, Activation, Batch Normalization and Dropout."""
+
+    def __init__(
+        self,
+        in_dim: int,
+        out_dim: int,
+        use_batch_norm: bool = True,
+        dropout: float = 0,
+        activation: str = "ReLU",
+    ):
+        """
+        Args:
+            in_dim (int): Input dimension of `torch.nn.Linear` layer.
+            out_dim (int): Output dimension of `torch.nn.Linear` layer.
+            use_batch_norm (bool, optional): Whether to use Batch Normalization (BN) after activation. Defaults to True.
+            dropout (float, optional): Dropout probability (used after BN). Defaults to 0.
+            activation (str, optional): Type of activation function used before BN. Defaults to 0.
+        """
         super().__init__()
         self.use_batch_norm = use_batch_norm
         self.use_dropout = dropout > 0
 
         self.linear = nn.Linear(in_dim, out_dim)
-        self.relu = nn.ReLU()
+        self.activation = getattr(nn, activation)()
         if self.use_batch_norm:
             self.batch_norm = nn.BatchNorm1d(out_dim)
 
@@ -19,7 +36,7 @@ class FeedForwardBlock(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.linear(x)
-        out = self.relu(out)
+        out = self.activation(out)
         if self.use_batch_norm:
             out = self.batch_norm(out)
         if self.use_dropout:
@@ -28,16 +45,36 @@ class FeedForwardBlock(nn.Module):
 
 
 class MLP(FeatureExtractor):
+    """Multi Layer Perceptron (MLP) constructed of many FeedForward blocks."""
+
     name: str = "MLP"
 
-    def __init__(self, in_dim, hidden_dims):
+    def __init__(
+        self,
+        in_dim: int,
+        hidden_dims: list[int],
+        use_batch_norm: bool,
+        dropout: float = 0,
+        activation: str = "ReLU",
+    ):
+        """
+        Args:
+            in_dim (int): Input dimension.
+            hidden_dims (list[int]): Dimensionalities of hidden layers.
+            use_batch_norm (bool, optional): Whether to use Batch Normalization (BN) after activation. Defaults to True.
+            dropout (float, optional): Dropout probability (used after BN). Defaults to 0.
+            activation (str, optional): Type of activation function used before BN. Defaults to 0.
+        """
         super().__init__()
         self.hidden_dims = hidden_dims
         in_dims = [in_dim] + hidden_dims[:-1]
         n_layers = len(hidden_dims)
         layers: list[nn.Module] = [
             nn.Flatten(start_dim=1, end_dim=-1),
-            *[FeedForwardBlock(in_dims[i], hidden_dims[i]) for i in range(n_layers)],
+            *[
+                FeedForwardBlock(in_dims[i], hidden_dims[i], use_batch_norm, dropout, activation)
+                for i in range(n_layers)
+            ],
         ]
         self.net = nn.Sequential(*layers)
 
